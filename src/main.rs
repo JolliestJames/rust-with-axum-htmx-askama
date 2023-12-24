67eba856437159cd3f9cd21c8eea4d6061d8f556
jame <cloudismydog@gmail.com>
2023-12-24 13:30:25 -0800

    implement HelloTemplate, initialize router and listen on port 8000

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,41 @@
+use askama::Template;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Router,
+};
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+async fn hello() -> impl IntoResponse {
+    let template = HelloTemplate {};
+    HtmlTemplate(template)
+}
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate;
+
+struct HtmlTemplate<T>(T);
+
+impl<T> IntoResponse for HtmlTemplate<T>
+where T: Template,
+{
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}, err", err),
+            )
+                .into_response(),
+        }
+    }
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -11,5 +44,16 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    info!("hello, web server!");
+    info!("initializing router");
+
+    let router = Router::new().route("/", get(hello));
+    let port = 8000_u16;
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port)).await.unwrap();
+    axum::serve(listener, router.into_make_service())
+        .await
+        .unwrap();
+
+    info!("router initialized, now listening on port {}", port);
+
+    Ok(())
 }
