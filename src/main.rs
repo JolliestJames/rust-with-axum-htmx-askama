1092f21f66745172f1ca1e76fd6c1f84105a047b
jame <cloudismydog@gmail.com>
2023-12-24 14:09:42 -0800

    serve assets

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use axum::{
     routing::get,
     Router,
 };
+use tower_http::services::ServeDir;
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -46,9 +47,16 @@ async fn main() -> anyhow::Result<()> {
 
     info!("initializing router");
 
-    let router = Router::new().route("/", get(hello));
+    let assets_path = std::env::current_dir().unwrap();
     let port = 8000_u16;
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port)).await.unwrap();
+    let router = Router::new()
+        .route("/", get(hello))
+        .nest_service(
+            "/assets",
+            ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())),
+        );
+
     axum::serve(listener, router.into_make_service())
         .await
         .unwrap();
